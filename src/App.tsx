b4e58d0f2e9487f9928da68c76650c97478f526e
Olatunji Muyiwa Ishola <olatunjimuyiwa509@gmail.com>
2026-08-10 04:14:54 +0100

    Build a Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Button } from "@chakra-ui/react";
-
+import { Grid, GridItem } from "@chakra-ui/react";
 function App() {
-  return <Button colorPalette="blue">Primary</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, // Mobile view
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
