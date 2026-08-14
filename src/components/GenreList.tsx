543d08690d636a2601ad9946844a1a7d47e8f8b8
Olatunji Muyiwa Ishola <olatunjimuyiwa509@gmail.com>
2026-08-14 21:40:51 +0100

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,4 @@
-
-import { HStack, Button, List, Spinner, Text } from "@chakra-ui/react";
+import { HStack, Button, List, Spinner, Text, Heading } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import type { Genre } from "../hooks/useGenres";
 import GenreImage from "./GenreImage";
@@ -16,26 +15,31 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List.Root variant="plain">
-      {data.map((genre) => (
-        <List.Item key={genre.id} paddingY="5px">
-          <HStack>
-            <GenreImage genreId={genre.id} genreName={genre.name} />
+    <>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
+      <List.Root variant="plain">
+        {data.map((genre) => (
+          <List.Item key={genre.id} paddingY="5px">
+            <HStack objectFit={"cover"}>
+              <GenreImage genreId={genre.id} genreName={genre.name} />
 
-            <Button
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-              onClick={() => onSelectGenre(genre)}
-              variant="ghost"
-              fontSize="md"
-              whiteSpace="normal"
-              textAlign="left"
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+              <Button
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                variant="ghost"
+                fontSize="md"
+                whiteSpace="normal"
+                textAlign="left"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
