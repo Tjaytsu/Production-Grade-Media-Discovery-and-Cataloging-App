6337464e69ff78e25dbcd83a0a87b7df117bffa2
Olatunji Muyiwa Ishola <olatunjimuyiwa509@gmail.com>
2026-08-14 22:16:11 +0100

    Ready for Deployment

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,6 @@
 // src/services/image-url.ts
 
 import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
-
-const NO_IMAGE = "https://placehold.co/600x400/2a2a2a/ffffff?text=No+Image";
-
 export const getCroppedImageUrl = (
   path: string | null,
   size: "w300" | "w500" | "w780" = "w300"
